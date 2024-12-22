5eb835aaec792848658c265fc2ffab6e2e6b62a5
auriane90 <77453577+Auriane90@users.noreply.github.com>
2024-12-22 07:32:12 -0300

    Update reColorButtonStyle.swift

--- reMind/reMind/Utils/Styles/reColorButtonStyle.swift
+++ reMind/reMind/Utils/Styles/reColorButtonStyle.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 
 struct reColorButtonStyle: ButtonStyle {
-    @State var theme: reTheme
+    private var theme: reTheme
+    private var opacity: Double
 
-    init(_ theme: reTheme) {
+    init(_ theme: reTheme, _ opacity: Double = 1) {
         self.theme = theme
+        self.opacity = opacity
     }
 
     func makeBody(configuration: Configuration) -> some View {
@@ -19,7 +21,7 @@ struct reColorButtonStyle: ButtonStyle {
             .padding()
             .font(.body)
             .fontWeight(.bold)
-            .foregroundColor(Palette.reBlack.render)
+            .foregroundColor(Palette.reBlack.render.opacity(opacity))
             .frame(height: 48)
             .background {
                 Rectangle()
