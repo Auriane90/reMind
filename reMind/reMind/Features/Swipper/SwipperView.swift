dea4070a19c7b07ea3b582e26138883e068ad92d
auriane90 <77453577+Auriane90@users.noreply.github.com>
2024-12-22 06:48:38 -0300

    Update SwipperView.swift

--- reMind/reMind/Features/Swipper/SwipperView.swift
+++ reMind/reMind/Features/Swipper/SwipperView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct SwipperView: View {
+    @EnvironmentObject var router: reAppRouter
     @State var review: SwipeReview
     @State private var direction: SwipperDirection = .none
 
@@ -20,15 +21,18 @@ struct SwipperView: View {
 
             SwipperCard(direction: $direction,
                 frontContent: {
-                    Text("Term")
+                    Text(review.termsToReview.first?.value ?? "")
+                        .foregroundColor(Palette.reBlack.render)
                 }, backContent: {
-                    Text("Meaning")
+                    Text(review.termsToReview.first?.meaning ?? "")
+                        .foregroundColor(Palette.reBlack.render)
                 })
 
             Spacer()
 
             Button(action: {
-                print("finish review")
+                router.navigate(to: .SwipperReport(report: review.report,
+                                                   termsToReview: review.count))
             }, label: {
                 Text("Finish Review")
                     .frame(maxWidth: .infinity, alignment: .center)
@@ -42,24 +46,62 @@ struct SwipperView: View {
         .background(reBackground())
         .navigationTitle("\(review.termsToReview.count) terms left")
         .navigationBarTitleDisplayMode(.inline)
+        .onChange(of: direction){ newValue in
+            switch newValue{
+            case .right:
+                review.correct()
+            case .left:
+                review.wrong()
+            default:
+                break
+            }
+        }
+        .onChange(of: review){ _ in
+            if review.termsToReview.isEmpty{
+                router.navigate(to: .SwipperReport(report: review.report,
+                                                   termsToReview: review.count))
+            }
+        }
     }
 }
 
 struct SwipperView_Previews: PreviewProvider {
-    static let term: Term = {
-        let term = Term(context: CoreDataStack.inMemory.managedContext)
-        term.value = "Term"
-        term.meaning = "Meaning"
-        term.rawSRS = 0
-        term.rawTheme = 0
+    
+    @StateObject static var router = reAppRouter(navigationPath: .init())
+    
+    static var termsToReview = {
+        var termsToReview: [Term] = []
+        let term1 = Term(context: CoreDataStack.inMemory.managedContext)
+        term1.value = "Wrong"
+        term1.meaning = "Meaning goes here..."
+        term1.rawSRS = 1
         
-        return term
-    }()
+        
+        let term2 = Term(context: CoreDataStack.inMemory.managedContext)
+        term2.value = "Wrong2"
+        term2.meaning = "Meaning goes here 2"
+        term2.rawSRS = 1
+        
+        
+        let term3 = Term(context: CoreDataStack.inMemory.managedContext)
+        term3.value = "Front Value"
+        term3.meaning = "Meaning"
+        term3.rawSRS = 1
+        
+        let term4 = Term(context: CoreDataStack.inMemory.managedContext)
+        term4.value = "Front Value 2"
+        term4.meaning = "Description 2"
+        term4.rawSRS = 1
+        
+        termsToReview.append(contentsOf: [term1, term2, term3, term4])
+        return termsToReview
+    } ()
+    
+    static var swipeReview = SwipeReview(termsToReview: termsToReview)
     static var previews: some View {
-        NavigationStack {
-            SwipperView(review: SwipeReview(termsToReview: [
-                Term(context: CoreDataStack.inMemory.managedContext)
-            ]))
+        NavigationStack (path: $router.navigationPath){
+            SwipperView(review: SwipperView_Previews.swipeReview)
         }
+        .environmentObject(router)
     }
 }
